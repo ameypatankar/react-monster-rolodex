eb4bf3be0ae6f8895afa31b022855f7e29cb7c0e
Amey <amey@astechsolutions.in>
2020-06-06 00:50:44 +0530

    Updates

---
+++ precache-manifest.192794245e16f5d6a06b46c146681f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c0e29f5e63070441eb6aed26819a95",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b9281ec3f2fe7781ea6b",
+    "url": "/react-monsters-rolodex/static/css/main.9fd82688.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react-monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9281ec3f2fe7781ea6b",
+    "url": "/react-monsters-rolodex/static/js/main.5c3d663a.chunk.js"
+  },
+  {
+    "revision": "3941f1e8f8f4232c2c98",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.bbfcc21c.js"
+  }
+]);
